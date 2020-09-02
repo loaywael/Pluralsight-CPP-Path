b04e7fb37d58c786672a3049f93e3e1466fdf515
loaywael <hythamconn1@gmail.com>
2020-09-03 01:08:26 +0200

    Added Move Semantics

--- Intermediate/Introduction to Algorithms and Data structures/Data Structures/Arrays/MyArray.hpp
+++ Intermediate/Introduction to Algorithms and Data structures/Data Structures/Arrays/MyArray.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-
+#include <algorithm>
 
 template <class T>
 class MyArray {
@@ -32,4 +32,58 @@ public:
             throw std::out_of_range("index is out of range");
         }
     }    
+
+    // copy constructor for deep copy and double delete bug prevention
+    MyArray(MyArray& srcObj) {
+        if (!srcObj.isEmpty()) {
+            std::cout << "a copy were constructed\n";
+            arr_size = srcObj.size();
+            val_ptr = new T [arr_size]{};
+            for (int i{0}; i < srcObj.arr_size; i++) {
+                val_ptr[i] = srcObj[i];
+            }
+        }
+    }
+
+    // copy swap idiom -- cleaner implementation
+    friend void swap_arrays(MyArray& arr1, MyArray& arr2) noexcept {
+        std::swap(arr1.arr_size, arr2.arr_size);
+        std::swap(arr1.val_ptr, arr2.val_ptr);
+        // destructor is called after exiting the function on src copy
+    }
+
+    // cleaner implementation of copy assignment overloading
+    // that automatically calls move constructors
+    MyArray& operator=(MyArray srcObjCpy) noexcept{ 
+        swap_arrays(*this, srcObjCpy);
+        return *this;
+        // destructor is called after exiting the function on src copy
+    }
+
+    // longer implementation of copy assignment overloading
+    // MyArray& operator=(MyArray& srcObj) noexcept{ 
+    //     // prevents x = x
+    //     if (this != &srcObj) {
+    //         // release previous array blocks if any
+    //         if (!this->isEmpty()) {
+    //             delete[] val_ptr;  
+    //             arr_size = srcObj.size();
+    //             val_ptr = new T [arr_size];
+    //         }
+    //         // implement deep copy from src to this
+    //         for (int i{0}; i < srcObj.size(); i++) {
+    //             val_ptr[i] = srcObj[i];
+    //         }
+    //     }
+    //     return *this;
+    // }
+
+    MyArray(MyArray&& src) noexcept{
+        std::cout << "a move were constructed\n";
+        arr_size = src.size();
+        val_ptr = src.val_ptr;
+        src.arr_size = 0;
+        src.val_ptr = nullptr;
+    }
 };
+
--- Intermediate/Introduction to Algorithms and Data structures/Data Structures/Arrays/main.cpp
+++ Intermediate/Introduction to Algorithms and Data structures/Data Structures/Arrays/main.cpp
@@ -14,12 +14,33 @@ std::ostream& operator<<(std::ostream& os, MyArray<T>& arr) {
 }
 
 int main() {
-    MyArray<int> arr1{7};
+    MyArray<int> arr1{1};
     std::cout << arr1.size() << std::endl;
 
     MyArray<int> arr2{3};
     std::cout << arr2.size() << std::endl;
     arr2[2] = 25;
     std::cout << arr2;
+
+    MyArray<int> arr3 = arr2;
+    std::cout << "size arr3: " << arr3.size() << "\tarr3 val: " << arr3;
+    
+    MyArray<int> arr4(11);
+    std::cout << arr4;
+    arr4 = arr3;
+    std::cout << arr4;
+
+    MyArray<int> arr5{9};
+    std::cout << arr5;
+    swap_arrays(arr5, arr1);
+    std::cout << "arr5: " << arr5 << "arr1: " << arr1;
+
+    MyArray<int> arr6(std::move(arr1));
+    std::cout << arr6;
+
+    MyArray<int> arr7;
+    arr7 = std::move(arr2);
+    std::cout << "arr7: " << arr7 << "arr2: " << arr2;
+    
     return 0;
 }
